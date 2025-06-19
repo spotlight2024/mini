dd2be3d70185e9fc20496922c961fb4010a71cb3
gongcong <gongcong@example.com>
2025-06-19 15:34:14 +0800

    删除 WebSocket 相关模块，包括服务器、消息处理器、管理器及示例文件，简化代码结构，移除不再使用的功能。

--- script/websocket/__init__.py
+++
@@ -1,5 +0,0 @@
-from .server import WebSocketServer
-from .message_handler import MessageType, Message
-from .manager import WebSocketManager
-
-__all__ = ['WebSocketServer', 'MessageType', 'Message', 'WebSocketManager'] 
