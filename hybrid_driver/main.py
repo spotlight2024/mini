29202f8897d476d6faca9c8e1400846c50bcf530
gongcong <gongcong@example.com>
2025-06-25 17:04:01 +0800

    [feature] : change server ip

--- hybrid_driver/main.py
+++ hybrid_driver/main.py
@@ -6,4 +6,4 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="172.16.1.125", port=8000, reload=True)
+    uvicorn.run("server:app", host="47.94.130.125", port=8000, reload=True)
